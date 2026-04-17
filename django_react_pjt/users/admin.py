fadf9e8dcf4765fb0410db6530c2f0f67a3465ca
JodelDjan <w1973296@westminster.ac.uk>
2026-04-17 14:51:18 +0100

    changed admin.py to handle [assword hashing automatically

--- django_react_pjt/users/admin.py
+++ django_react_pjt/users/admin.py
@@ -1,21 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, ResearcherProfile, GeneralProfile
+
+#Handles password hashing from admin panel using built in UserAdmin form
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display  = ['email', 'first_name', 'last_name', 'role', 'is_staff']
+    ordering      = ['email']
+    fieldsets = (
+        (None,           {'fields': ('email', 'password', 'role')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions',  {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'role', 'password1', 'password2'),
+        }),
+    )
+
 
 admin.site.register(CustomUser) 
 admin.site.register(ResearcherProfile) 
 admin.site.register(GeneralProfile) 
-
-
-
-# # Register your models here.
-# class CustomUserAdmin(UserAdmin):
-#     add_form = CustomUserCreationForm
-#     form = CustomUserChangeForm
-#     model = CustomUser
-#     list_display = [
-#         "email",
-#         "username",
-#         "is_staff",
-#         "is_active",
-#     ]
 
