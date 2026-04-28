f05cfd625ea0416e81aeea658822a964c5133672
JodelDjan <w1973296@westminster.ac.uk>
2026-04-28 19:05:12 +0100

    installed bootstrap

--- django_react_pjt/frontend/src/main.jsx
+++ django_react_pjt/frontend/src/main.jsx
@@ -1,3 +1,4 @@
+//import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
@@ -11,6 +12,11 @@ import EditProfile from "./pages/EditProfile"
 import ApplicationsDashboard from "./pages/ApplicationsDashboard"
 import Bookmarks      from "./pages/Bookmarks"
 import Notifications  from "./pages/Notifications"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
